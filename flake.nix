c546201aff6f8ae2eebe20f820df2b246f3f97d2
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-04-10 20:49:14 +0300

    fix: resolve rebase conflict in flake.nix
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -8,37 +8,7 @@
 
   outputs =
     { nixpkgs, flake-utils, ... }:
-<<<<<<< HEAD
-    let
-      targets = {
-        "x86_64-linux" = "x86_64-unknown-linux-gnu";
-        "aarch64-darwin" = "aarch64-apple-darwin";
-        "x86_64-darwin" = "x86_64-apple-darwin";
-      };
-      hashes = {
-        "x86_64-linux" = "sha256-WCL5ry5lp+cwjQJplaJVonLKzOGcbPBLxh2dt5Yomlk=";
-        "aarch64-darwin" = "sha256-jR0pzIHjjcnL4kgHeAmhLCl1kqQqKnvzaQ2puibXpSk=";
-        "x86_64-darwin" = "sha256-vBsVX9lfSPQCa9QGztN50gmzl0Xj2sFGtVIwA6epqLU=";
-      };
-    in
-    flake-utils.lib.eachSystem (builtins.attrNames targets) (
-||||||| parent of ab44390 (ci: migrate to nix-based builds and flake.lock management)
-    let
-      targets = {
-        "x86_64-linux" = "x86_64-unknown-linux-gnu";
-        "aarch64-darwin" = "aarch64-apple-darwin";
-        "x86_64-darwin" = "x86_64-apple-darwin";
-      };
-      hashes = {
-        "x86_64-linux" = "sha256-nyeCeWNf58oDlu1wtWF/ZbhvtKG38JzZqrKAmswgGgw=";
-        "aarch64-darwin" = "sha256-LBKfP/zgpWHB+rcXsfFGcUwzNwSEM5TaAaE/atwKLAg=";
-        "x86_64-darwin" = "sha256-/KS+Vain0bImm6ms4v4jRLzfE5hYKDY7Lqt9IjQOFVg=";
-      };
-    in
-    flake-utils.lib.eachSystem (builtins.attrNames targets) (
-=======
     flake-utils.lib.eachDefaultSystem (
->>>>>>> ab44390 (ci: migrate to nix-based builds and flake.lock management)
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
